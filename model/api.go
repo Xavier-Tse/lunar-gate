d8f6455cf4f5017e33385f7e30169362ebe980b7
XavierTse <xaviertse1024@gmail.com>
2025-05-15 21:46:55 +0800

    feat: api删除同步api角色记录

--- model/api.go
+++ model/api.go
@@ -1,9 +1,21 @@
 package model
 
+import (
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
 type Api struct {
 	LunarModel
 	Name   string `gorm:"size:32" json:"name"`
 	Path   string `gorm:"size:255" json:"path"`
 	Method string `gorm:"size:16" json:"method"`
 	Group  string `gorm:"size:32" json:"group"`
+}
+
+func (a Api) BeforeDelete(tx *gorm.DB) error {
+	var list []RoleApi
+	err := tx.Find(&list, "api_id = ?", a.ID).Delete(&list).Error
+	logrus.Infof("删除api %d ，关联删除角色api %d 个", a.ID, len(list))
+	return err
 }
--- model/role_api.go
+++ model/role_api.go
@@ -1,9 +1,23 @@
 package model
 
+import (
+	"fmt"
+	"github.com/Xavier-Tse/lunar-gate/global"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
 type RoleApi struct {
 	LunarModel
 	ApiID  uint `json:"apiID"`
 	Api    Api  `gorm:"foreignKey:ApiID" json:"-"`
 	RoleID uint `json:"roleID"`
 	Role   Role `gorm:"foreignKey:RoleID" json:"role"`
+}
+
+func (a RoleApi) BeforeDelete(tx *gorm.DB) error {
+	tx.Preload("Api").Take(&a)
+	global.Casbin.RemovePolicy(fmt.Sprintf("%d", a.RoleID), a.Api.Path, a.Api.Method)
+	logrus.Infof("删除角色api %d %s %s", a.RoleID, a.Api.Path, a.Api.Method)
+	return nil
 }
